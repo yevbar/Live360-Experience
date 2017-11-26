ed278a7eca1626148a6ae23ddff42fcfe2814053
Ubuntu <ubuntu@ip-172-31-29-183.us-west-2.compute.internal>
2017-11-26 22:22:45 +0000

    Further developed the site

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "Hello from Flask!"
+	return render_template("home/index.html")
+
+@app.route("/find")
+def find():
+	return render_template("find/index.html")
 
 if __name__ == "__main__":
 	app.run()
